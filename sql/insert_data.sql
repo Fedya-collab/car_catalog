8831cca163cb5fabf000b9f16b85c9748b1826c7
Fedya-collab <d5443450069@gmail.com>
2026-03-07 22:23:29 +0300

    feat: add 7 more car records for analysis

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -1,4 +1,11 @@
 INSERT INTO cars (brand, model, year, color, price) VALUES
 ('Toyota', 'Camry', 2020, 'Silver', 1500000.00),
 ('BMW', 'X5', 2022, 'Black', 4500000.50),
-('Lada', 'Vesta', 2021, 'White', 800000.00);
+('Lada', 'Vesta', 2021, 'White', 1800000.00),
+('Ford', 'Focus', 2019, 'Blue', 1600000.00),
+('Hyundai', 'Solaris', 2021, 'Gray', 950000.00),
+('Kia', 'Rio', 2022, 'Red', 1250000.00),
+('Mercedes', 'G-Class', 2023, 'Black', 6500000.00),
+('Audi', 'A4', 2020, 'White', 2800000.00),
+('Volkswagen', 'Polo', 2021, 'Silver', 1200000.00),
+('Nissan', 'GT-R', 2018, 'Green', 2100000.00);
